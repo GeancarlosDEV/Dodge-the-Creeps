72cb842695d3be7dcf84258a8a39de3f1ad14399
Gean Carlos <ggean@live.com>
2022-04-23 10:49:57 -0300

    Jogo concluído

---
+++ Main.gd
@@ -0,0 +1,45 @@
+extends Node
+
+export(PackedScene) var mob_scene
+var score
+
+func _ready():
+	randomize()
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+func _on_MobTimer_timeout():
+	var mob = mob_scene.instance()
+	var mob_spaw_location = get_node("MobPath/MobSpawnLocation")
+	mob_spaw_location.offset = randi()
+	var direction = mob_spaw_location.rotation + PI / 2
+	mob.position = mob_spaw_location.position
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	var velocity = Vector2(rand_range(150.0, 250.0),0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	add_child(mob)
---
+++ README.md
@@ -0,0 +1,8 @@
+<h1 align="center"> DODGE THE CREEPS </h1>
+<h1 align="center"> JOGO 2D </h1>
+
+
+<p align="justify"> jOGO CONCLUÍDO </p>
+
+
+> Status do Projeto: Concluido :heavy_check_mark:
